8d440f67e22ab3939538e05aa05d535abc53491d
Magesh <tkmagesh77@gmail.com>
2018-02-26 12:23:52 +0530

    products assignment

---
+++ products.js
@@ -0,0 +1,13 @@
+var products = [
+	{id : 6, name : 'Pen', cost : 50, units : 20, category : 'stationary'},
+	{id : 9, name : 'Ten', cost : 70, units : 70, category : 'stationary'},
+	{id : 3, name : 'Len', cost : 60, units : 60, category : 'grocery'},
+	{id : 5, name : 'Zen', cost : 30, units : 30, category : 'grocery'},
+	{id : 1, name : 'Ken', cost : 20, units : 80, category : 'utencil'},
+];
+
+//filter, map, reduce, forEach
+
+1. filter all stationary products
+2. find the sum of product values (product value = cost * units)
+3. create a new product list with the cost increased by 10%
