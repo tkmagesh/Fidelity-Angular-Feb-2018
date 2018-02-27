351b6a14519b9ce9df0cc522645abcc0f41ebc38
Magesh <tkmagesh77@gmail.com>
2018-02-27 15:33:36 +0530

    bugOperations Service

--- bugTrackerApp/src/app/bugTracker/bugTracker.component.ts
+++ bugTrackerApp/src/app/bugTracker/bugTracker.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Bug } from './models/Bug';
+import { BugOperationsService } from './services/bugOperations.service';
 
 @Component({
 	selector : 'app-bug-tracker',
@@ -8,15 +9,18 @@ import { Bug } from './models/Bug';
 export class BugTrackerComponent{
 	bugs : Bug[] = [];
 
+	
+
+	constructor(private bugOperations : BugOperationsService){
+		
+	}
+
 	onCreateNewClick(bugName : string){
-		let newBug : Bug = {
-			name : bugName,
-			isClosed : false
-		};
+		let newBug : Bug = this.bugOperations.createNew(bugName);
 		this.bugs.push(newBug);
 	}
 	onBugNameClick(bug:Bug){
-		bug.isClosed = !bug.isClosed;
+		this.bugOperations.toggle(bug);
 	}
 
 	onRemoveClosedClick(){
